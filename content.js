86a2b154e0308313d367071b711ea4a5e358ceee
Gabriel Elias <gabriel_elias9@hotmail.com>
2022-05-02 18:11:52 -0300

    Error corrected

--- content.js
+++ content.js
@@ -1,11 +1,15 @@
 const div = document.createElement('div')
 const styled = document.createElement('style')
 const popup = document.createElement('div')
+const btnlimpar = document.createElement('button')
+const input = document.createElement('input')
 
 
 document.body.append(div)
 document.body.append(popup)
 document.body.append(styled)
+document.body.append(input)
+document.body.append(btnlimpar)
 
 
 const divConfigs = { 
@@ -13,7 +17,17 @@ const divConfigs = {
     title: 'Scrapping Urls', 
     class : 'getid', 
     style :  ` 
-
+        .input_dom_popup { 
+            z-index: 5;
+            position: fixed;
+            bottom: 100px;
+            background: #fff;
+            left: 25px;
+            font-size: 15px;
+            color: #000;
+            border-radius: 5px;
+            padding: 16px 4px;
+        }
         .popupDiv { 
            
             width: 100%;
@@ -32,7 +46,7 @@ const divConfigs = {
 
         }
         .getid { 
-            z-index: 999;
+            z-index: 5;
             position: fixed;
             bottom: 33px;
             background: #979595;
@@ -49,6 +63,10 @@ const divConfigs = {
             .activePopup {
                 display: none;
             }
+            .btn-limpar {
+                left: 210px;
+                border: none;
+            }
             ` ,
 
 }
@@ -56,12 +74,27 @@ const divConfigs = {
 
 div.innerHTML = divConfigs.title
 
+btnlimpar.innerHTML = 'Reset'
+
 div.classList = divConfigs.class
 
+input.classList = 'input_dom_popup'
+
+btnlimpar.classList = 'getid btn-limpar'
 
 popup.classList = 'popupDiv activePopup'
 
 styled.innerHTML = divConfigs.style
+
+novo = new Array();
+
+btnlimpar.addEventListener('click', ()=> {
+    localStorage.clear();
+    location.href = 'https://google.com/'
+}) 
+
+input.setAttribute('placeholder', 'URL completa')
+input.setAttribute('title', 'Preencha este campo, se você for pesquisar por uma palavra especifica')
 
 
 div.addEventListener('click', ()=> {
@@ -71,8 +104,25 @@ div.addEventListener('click', ()=> {
         div.innerHTML = 'Scrapping'
 
         t = document.querySelector("#tsf > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input").value
-        
-        search = 'https://' + t.substr(5, t.length);
+        query = t.replaceAll(' ', '+')
+
+        if(input.value != ''){
+
+            localStorage.setItem('base', input.value)
+
+        }
+
+        if(localStorage.getItem('base')){
+
+            search = localStorage.getItem('base')
+            
+        }
+        else { 
+
+            search = 'https://' + t.substr(5);
+
+        }
+
         
         a = document.querySelectorAll('a')
 
@@ -81,7 +131,7 @@ div.addEventListener('click', ()=> {
         if(!localStorage.getItem('index') || localStorage.getItem('index') == NaN){
 
             localStorage.setItem('index', index)
-
+            
         } 
 
         arr = []
@@ -91,21 +141,40 @@ div.addEventListener('click', ()=> {
             link = l.getAttribute('href')
         
             if(typeof(link) == 'string') {
-        
+
                 if(link.includes(search) && !link.includes('webcache') && !link.includes('view')){
                     
 
                     arr.push(link)
-                   
+                    
                     // localStorage.setItem('links')
-
                 }
                 
             }
             
         })
 
-        arr.push(localStorage.getItem('links'))
+        if(localStorage.getItem('links')){
+
+            valores = localStorage.getItem('links');
+
+            vals = valores.replaceAll(/[\\"]/g, '');
+
+            auxmax = vals.split(',');
+
+            novo = auxmax.filter((found, indice) => {
+                return auxmax.indexOf(found) === indice
+            })
+
+            arr.push(JSON.stringify(novo))
+
+        }
+        else {
+
+            arr.push(JSON.stringify(arr))
+
+        }
+       
 
         localStorage.setItem('links', JSON.stringify(arr))
 
@@ -114,26 +183,20 @@ div.addEventListener('click', ()=> {
         localStorage.setItem('index', aux.toString());
 
 
-        base = `https://www.google.com/search?q=${t}&rlz=1C1GCEU_pt-BRBR965BR965&ei=WkRsYtejEsOV0AaroaqoCw&start=${aux}&sa=N&ved=2ahUKEwjXuOuviLr3AhXDCtQKHauQCrUQ8tMDegQIARA_&biw=1366&bih=625&dpr=1`
+        base = `https://www.google.com/search?q=${query}&rlz=1C1GCEU_pt-BRBR965BR965&ei=WkRsYtejEsOV0AaroaqoCw&start=${aux}&sa=N&ved=2ahUKEwjXuOuviLr3AhXDCtQKHauQCrUQ8tMDegQIARA_&biw=1366&bih=625&dpr=1`
         
 
 
         if(document.body.innerText.includes('não encontrou nenhum documento correspondente')){
 
-            valores = localStorage.getItem('links');
-
-            vals = valores.replace(/[\\"]/g, '');
-
-            novoarray = vals.split(',')
-
-            novoarray.forEach(n => {
+            novo.forEach((n, i) => {
                 
-                t = n.replace('[', '')
-                x = t.replace('null', '')
-                j = x.replace(']', '')
-                h = j.replace('[', '')
+                t = n.replaceAll('[', '')
+                x = t.replaceAll('null', '')
+                j = x.replaceAll(']', '')
                 
-                popup.innerHTML += h + '<br>'
+
+                popup.innerHTML += j + '<br>'
 
             })
             localStorage.clear();
@@ -141,7 +204,9 @@ div.addEventListener('click', ()=> {
         }
         else {
 
-            location.href = base;
+        location.href = base;
+
+
 
         }
 
